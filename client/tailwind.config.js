99d61fe57a98d0f60ecc4574a1146af2dd12e5c3
mactav1sh <mactavish104@gmail.com>
2023-02-11 17:41:27 +0200

    updated project settings

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        home: "url('/src/assets/imgs/sun-tornado.png')",
+        headphone: "url('/src/assets/imgs/headphone.png')",
+      },
+      fontFamily: {
+        logo: ['"Exo 2"', 'sans-serif'],
+      },
+      colors: {
+        'brand-pink': {
+          25: '#fff2f5',
+          50: '#ffe9ee',
+          100: '#ffbecc',
+          200: '#ff93ab',
+          300: '#ff6789',
+          400: '#ff3c67',
+          500: '#ff2656',
+          600: '#e6224d',
+          700: '#cc1e45',
+          800: '#b31b3c',
+          900: '#80132b',
+          1000: '#4c0b1a',
+        },
+      },
+    },
   },
   plugins: [],
 };
