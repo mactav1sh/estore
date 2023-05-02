8420c67076a4642a47c3d7fa68de2e419cc989d8
mactav1sh <mactavish104@gmail.com>
2023-05-02 20:04:05 +0300

    added products pagination

--- client/src/features/misc/routes/LoadingPage.tsx
+++ client/src/features/misc/routes/LoadingPage.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import Spinner from '../../../components/elements/Spinner';
 
 const LoadingPage = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-slate-200">
+    <div className="flex min-h-screen items-center justify-center bg-slate-50">
       <Spinner size="lg" />
     </div>
   );
--- client/src/features/products/routes/Products.tsx
+++ client/src/features/products/routes/Products.tsx
@@ -4,16 +4,25 @@ import LoadingPage from '../../misc/routes/LoadingPage';
 import ProductCard from '../components/ProductCard';
 import { useSearchParams } from 'react-router-dom';
 import { createQueryStr } from '../../../utils/createQueryStr';
+import { useEffect, useState } from 'react';
 
 const Products = () => {
+  const [page, setPage] = useState(1);
   const [params] = useSearchParams();
-  const queryStr = createQueryStr(params);
+
+  const limit = 12;
+  const queryStr = createQueryStr(params, page, limit);
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    console.log('scrolled to top');
+  }, [page]);
 
   const { isLoading, data } = useGetProducts(queryStr, queryStr);
 
   if (isLoading) return <LoadingPage />;
   return (
-    <main className="flex min-h-screen items-center justify-center bg-slate-50 px-2 pb-16 pt-32">
+    <main className="flex min-h-screen flex-col items-center justify-center space-y-12 bg-slate-50 px-2 pb-16 pt-32">
       {/* MAIN CONTENT */}
       {data.products && data.products.length > 0 ? (
         <div className="grid w-full grid-cols-1 justify-items-center gap-y-8 rounded-md p-2.5 md:w-auto md:grid-cols-2 md:gap-7 lg:grid-cols-4">
@@ -26,6 +35,27 @@ const Products = () => {
           No products found
         </p>
       )}
+      <div className="flex space-x-4">
+        <button
+          className="w-28 flex-1 rounded-sm bg-brand-pink-600 py-1 text-center text-sm font-semibold text-white shadow-sm duration-200  hover:shadow-md disabled:cursor-not-allowed disabled:bg-slate-500"
+          onClick={() => {
+            setPage((p) => p - 1);
+          }}
+          disabled={page === 1}
+        >
+          Previous
+        </button>
+
+        <button
+          className="w-28 flex-1 rounded-sm bg-brand-pink-600 py-1 text-center text-sm font-semibold text-white shadow-sm duration-200  hover:shadow-md disabled:cursor-not-allowed disabled:bg-slate-500"
+          onClick={() => {
+            setPage((p) => p + 1);
+          }}
+          disabled={!data.products || data.products.length < 12}
+        >
+          Next
+        </button>
+      </div>
     </main>
   );
 };
